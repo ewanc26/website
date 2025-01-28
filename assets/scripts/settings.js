5934983de43cfb6541acc7de0a2f55465c4c3d08
Ewan <156911937+ewanc26@users.noreply.github.com>
2025-01-28 21:16:18 +0000

    organisation

---
+++ assets/scripts/settings.js
@@ -0,0 +1,38 @@
+// docs: https://whtwnd.com/did:plc:xz3euvkhf44iadavovbsmqoo/3laxrz4dl4s2f
+atprotoSite({
+    PDS: 'altq.net',
+    title: "Ewan's Web Corner",
+    postsPerPage: 10,
+    pageLimit: 5,
+    linkToUserHandles: true,
+    showDrafts: false,
+    showReplies: true,
+    readMore: 5,
+    tagsAsClasses: true,
+    blobURL: undefined,
+    dateFormat: new Intl.DateTimeFormat(),
+    pages: {
+        'about': {
+            title: 'About Me',
+            post: 'at://did:plc:ofrbh253gwicbkc5nktqepol/com.whtwnd.blog.entry/3lgstfrk3ai2e'
+        },
+        'verification': {
+            title: 'Verification',
+            post: 'at://did:plc:ofrbh253gwicbkc5nktqepol/com.whtwnd.blog.entry/3lgt5skttvy2e'
+        }
+    },
+    users: [
+        {
+            DID: 'did:plc:ofrbh253gwicbkc5nktqepol',
+            handle: 'ewancroft.uk',
+            sources: {
+                Bluesky: true,
+                WhiteWind: true,
+                Frontpage: true,
+                PinkSea: false,
+                pastesphere: false,
+                Linkat: false
+            },
+        }
+    ],
+});
