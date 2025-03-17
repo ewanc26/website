e7197f5cda037c9aee1e3f22ac84a737a20221cd
Ewan Croft <git@ewancroft.uk>
2025-03-17 23:31:36 +0000

    add dockerfile

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,13 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-node';
+
+export default {
+  kit: {
+    adapter: adapter()
+  }
+};
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -14,5 +22,3 @@ const config = {
 		adapter: adapter()
 	}
 };
-
-export default config;
