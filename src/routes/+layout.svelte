8d12bbb1afb2f18a12491cc27c7ab2d4e4c04718
Ewan <git@ewancroft.uk>
2025-04-09 02:05:54 +0100

    style(layout): replace book icon with home icon in navigation
    The change updates the navigation icon from a book to a home icon for better clarity and user experience. This aligns with the intended purpose of the link, which directs users to the homepage.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -20,9 +20,9 @@
     <nav class="flex items-center box-border my-6">
         {#if $page.url.pathname !== '/'}
           <a href="/" class="font-medium text-[large] hover:text-[#b7e6c4]" aria-label="Home">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-book">
-              <path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20"/>
-              <path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z"/>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home">
+              <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+              <polyline points="9 22 9 12 15 12 15 22"></polyline>
             </svg>
           </a>
         {/if}
