aa698ace450739915c85612926660bfedae4bc47
Ewan <git@ewancroft.uk>
2025-04-20 18:34:03 +0100

    refactor(routes): reorder blog section in +page.svelte
    Move the blog section above the social media section to improve the logical flow of the page content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -26,6 +26,22 @@
 </svelte:head>
 
 <div class="mb-12 ml-4">
+    <h2 class="text-2xl font-bold mb-4 ml-2">Blog</h2>
+    <div class="grid grid-cols-[repeat(auto-fill,minmax(260px,1fr)_)] gap-x-4 gap-y-6 mx-2 my-6">
+        <div class="post-card">
+            <a href="https://blog.ewancroft.uk" target="_blank">
+                <div class="flex flex-col h-[110px] justify-between">
+                    <p class="text-link leading-[1.5] pb-2 title-truncate font-medium">Personal Blog</p>
+                    <div>
+                        <p class="text-muted text-sm">URL:</p>
+                        <p class="object-bottom text-primary">blog.ewancroft.uk</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
+</div>
+<div class="mb-12 ml-4">
     <h2 class="text-2xl font-bold mb-4 ml-2">Social Media</h2>
     <div class="grid grid-cols-[repeat(auto-fill,minmax(260px,1fr)_)] gap-x-4 gap-y-6 mx-2 my-6">
         <div class="post-card">
@@ -84,22 +100,6 @@
                     <div>
                         <p class="text-muted text-sm">Username:</p>
                         <p class="object-bottom text-primary">ewancroft.uk</p>
-                    </div>
-                </div>
-            </a>
-        </div>
-    </div>
-</div>
-<div class="mb-12 ml-4">
-    <h2 class="text-2xl font-bold mb-4 ml-2">Blog</h2>
-    <div class="grid grid-cols-[repeat(auto-fill,minmax(260px,1fr)_)] gap-x-4 gap-y-6 mx-2 my-6">
-        <div class="post-card">
-            <a href="https://blog.ewancroft.uk" target="_blank">
-                <div class="flex flex-col h-[110px] justify-between">
-                    <p class="text-link leading-[1.5] pb-2 title-truncate font-medium">Personal Blog</p>
-                    <div>
-                        <p class="text-muted text-sm">URL:</p>
-                        <p class="object-bottom text-primary">blog.ewancroft.uk</p>
                     </div>
                 </div>
             </a>
