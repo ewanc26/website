61003cb3c1b8835fcbc55fb604bb58f00fb07772
Ewan Croft <git@ewancroft.uk>
2025-04-21 07:40:51 +0100

    refactor(layout): remove favicon links from svelte:head
    The favicon links were removed from the layout file to simplify the HTML structure and avoid redundancy. The favicons are now managed elsewhere in the project.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,13 +7,6 @@
     let { data, children } = $props();
 </script>
 
-<svelte:head>
-    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
-    <link rel="manifest" href="/favicon/site.webmanifest">
-</svelte:head>
-
 <div class="box-border mx-auto px-4 sm:px-8 max-w-[1000px] pb-8">
     <nav class="flex items-center box-border my-6">
         {#if $page.url.pathname !== '/'}
