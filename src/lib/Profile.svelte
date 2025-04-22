36504501dbb385ab994dd363bc6c74858c4737a2
Ewan Croft <git@ewancroft.uk>
2025-04-22 04:43:20 +0100

    refactor(Profile.svelte): add conditional rendering for profile elements
    Add conditional rendering for profile banner, avatar, display name, and description to handle cases where these properties are undefined or null. This improves the component's robustness and user experience by preventing empty or broken UI elements.
    
    style: improve code readability in Profile.svelte
    
    Refactor the indentation and formatting to enhance readability and maintain consistency in the codebase.
    
    style(Profile.svelte): adjust margin and z-index for profile content
    
    Remove negative top margin and set margin-top to 0 to avoid overlap. Add z-index to ensure proper layering of elements.

--- src/lib/Profile.svelte
+++ src/lib/Profile.svelte
@@ -2,25 +2,37 @@
   const { profile } = $props();
 </script>
 
+{#if profile.banner}
+  <div
+    class="profile-banner p-4 relative rounded-[1em] m-2 mb-6"
+    style="background-image: url({profile.banner}); background-size: cover; background-position: center; min-height: 150px;"
+  ></div>
+{/if}
 <div
-  class="profile-banner p-4 relative rounded-[1em] m-2 mb-6"
-  style="background-image: url({profile.banner}); background-size: cover; background-position: center; min-height: 150px;"
-></div>
-<div
-  class="profile-content flex flex-col items-center justify-center text-center m-2 p-4 -mt-20 ml-4 mr-4 relative rounded-[1em]"
+  class="profile-content flex flex-col items-center justify-center text-center m-2 p-4 ml-4 mr-4 relative rounded-[1em]"
+  style="margin-top: 0; z-index: 1;"
 >
-  <img
-    src={profile.avatar}
-    alt="Bluesky avatar"
-    class="rounded-full w-32 h-32 -mt-2 shadow-lg hover:transform-none"
-  />
-  <div class="text-center p-3">
-    <h4 class="text-center">{profile.displayName}</h4>
-    <h6 class="text-center hover:underline">
-      <a href="https://bsky.app/profile/{profile.handle}" class="text-link hover:text-link-hover">@{profile.handle}</a>
-    </h6>
-  </div>
-  <div class="p-3 w-full text-center">
-    <p>{profile.description}</p>
-  </div>
+  {#if profile.avatar}
+    <img
+      src={profile.avatar}
+      alt="Bluesky avatar"
+      class="rounded-full w-32 h-32 -mt-2 shadow-lg hover:transform-none"
+    />
+  {/if}
+  {#if profile.displayName}
+    <div class="text-center p-3">
+      <h4 class="text-center">{profile.displayName}</h4>
+      <h6 class="text-center hover:underline">
+        <a
+          href="https://bsky.app/profile/{profile.handle}"
+          class="text-link hover:text-link-hover">@{profile.handle}</a
+        >
+      </h6>
+    </div>
+  {/if}
+  {#if profile.description}
+    <div class="p-3 w-full text-center">
+      <p>{profile.description}</p>
+    </div>
+  {/if}
 </div>
