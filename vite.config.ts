2b0be6500847005252308fd4286d94c95cc8e0eb
Ewan <git@ewancroft.uk>
2025-04-26 01:13:22 +0100

    refactor: update svelte and tailwind configs
    - Remove unnecessary newline in tailwind.config.ts
    - Change prerender origin in svelte.config.js and add CSP directives for enhanced security

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     plugins: [sveltekit()],
     build: {
-        target: 'esnext'
+        target: 'esnext',
+        minify: 'esbuild',
+        sourcemap: true
     }
 });
