5f7f3fda1e63f0441d4df6b3b539748af8fca15f
Ewan Croft <git@ewancroft.uk>
2025-04-29 17:11:28 +0100

    style(LinkCard): fix indentation for better readability
    The indentation in the LinkCard component was inconsistent, making the code harder to read. This commit aligns the indentation to improve code readability without changing the functionality.

--- src/lib/components/archive/LinkCard.svelte
+++ src/lib/components/archive/LinkCard.svelte
@@ -5,26 +5,26 @@
     export let title: string = "";
     export let url: string = "";
     export let value: string = "";
-  </script>
-  
-  <div 
-    class="post-card"
-    in:fly={{ 
-      y: 15, 
-      x: 0,
-      duration: 300, 
-      easing: quintOut 
-    }}
-  >
-    <a href={url}>
-      <div class="flex flex-col h-[110px] justify-between">
-        <p class="text-[var(--link-color)] leading-[1.5] pb-2 title-truncate font-medium" title={title}>{title}</p>
-        <div>
-          <p class="text-[var(--text-color)] text-sm">{value}</p>
-          <p class="object-bottom text-[var(--text-color)] text-sm">
-            {url?.replace(/^https?:\/\//, '').split('/')[0]}
-          </p>
-        </div>
+</script>
+
+<div 
+  class="post-card"
+  in:fly={{ 
+    y: 15, 
+    x: 0,
+    duration: 300, 
+    easing: quintOut 
+  }}
+>
+  <a href={url}>
+    <div class="flex flex-col h-[110px] justify-between">
+      <p class="text-[var(--link-color)] leading-[1.5] pb-2 title-truncate font-medium" title={title}>{title}</p>
+      <div>
+        <p class="text-[var(--text-color)] text-sm">{value}</p>
+        <p class="object-bottom text-[var(--text-color)] text-sm">
+          {url?.replace(/^https?:\/\//, '').split('/')[0]}
+        </p>
       </div>
-    </a>
-  </div>
+    </div>
+  </a>
+</div>
