f2d8fda29b2002942f182ef9b27a6e657cab0175
Ewan Croft <git@ewancroft.uk>
2025-07-18 07:27:43 +0100

    style(layout): improve footer component formatting and spacing

--- src/lib/components/layout/Footer.svelte
+++ src/lib/components/layout/Footer.svelte
@@ -21,29 +21,32 @@
     {#if profile?.handle}
       <a
         href="https://bsky.app/profile/{profile.did}"
-        class="text-[var(--link-color)] hover:text-[var(--link-hover-color)]">@{profile.handle}</a
+        class="text-[var(--link-color)] hover:text-[var(--link-hover-color)]"
+        >@{profile.handle}</a
       >
     {:else}
       {profile?.displayName || profile?.did}
     {/if}
-  </div>
-  <div class="mt-1">
-    powered by <a
+    <span class="mx-1">-</span> powered by
+    <a
       class="text-[var(--link-color)] hover:text-[var(--link-hover-color)]"
       href="https://atproto.com/guides/glossary#at-protocol">atproto</a
     >
     <span class="mx-1">-</span>
-    <a class="text-[var(--link-color)] hover:text-[var(--link-hover-color)]" href="/site/meta"
-      >More about this site</a
+    <a
+      class="text-[var(--link-color)] hover:text-[var(--link-hover-color)]"
+      href="/site/meta">More about this site</a
     >
-  </div>
-  <div class="mt-2">
+    <span class="mx-1">-</span>
     <LastCommit />
-  </div>
-  <div class="mt-2">
+    <span class="mx-1">-</span>
     <TidClock />
   </div>
   {#if env.PUBLIC_ACTIVITYPUB_USER && env.PUBLIC_ACTIVITYPUB_USER.length > 0}
-    <a rel="me" href={`https://${env.PUBLIC_ACTIVITYPUB_USER.split('@')[2]}/@${env.PUBLIC_ACTIVITYPUB_USER.split('@')[1]}`} aria-label="ActivityPub actor link."></a>
+    <a
+      rel="me"
+      href={`https://${env.PUBLIC_ACTIVITYPUB_USER.split("@")[2]}/@${env.PUBLIC_ACTIVITYPUB_USER.split("@")[1]}`}
+      aria-label="ActivityPub actor link."
+    ></a>
   {/if}
 </footer>
