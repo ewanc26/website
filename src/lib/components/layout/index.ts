59dbadcb785c1624166236a4ff3c5c1dafe1b86b
Ewan Croft <git@ewancroft.uk>
2025-07-20 18:58:25 +0100

    refactor(layout): restructure footer components
    - Move Footer component to footer/Main.svelte

--- src/lib/components/layout/index.ts
+++ src/lib/components/layout/index.ts
@@ -1,3 +1,3 @@
 export { default as Navigation } from "./Navigation.svelte";
-export { default as Footer } from "./Footer.svelte";
+export { default as Footer } from "./footer/Main.svelte";
 export { default as ThemeToggle } from "./ThemeToggle.svelte";
