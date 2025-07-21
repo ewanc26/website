96bc962bd2fd6249c7f566d77bab8fb3e6292d6e
Ewan Croft <git@ewancroft.uk>
2025-07-21 03:00:32 +0100

    feat(footer): improve layout and add visible ActivityPub user link
    Restructure footer into two rows for better readability and spacing. Make ActivityPub user link visible with formatted username instead of hidden aria-label. Update README to reflect footer component's functionality.

--- src/lib/components/layout/footer/Main.svelte
+++ src/lib/components/layout/footer/Main.svelte
@@ -15,29 +15,62 @@
   });
 </script>
 
-<footer class="text-center py-4 text-primary text-sm flex flex-wrap justify-center items-center gap-x-2 opacity-75">
-  &copy; <span id="copyright-year"></span>
-  {#if profile?.handle}
-    <a
-      href="https://bsky.app/profile/{profile.did}"
-      class="text-[var(--link-color)] hover:text-[var(--link-hover-color)]">@{profile.handle}</a
-    >
-  {:else}
-    {profile?.displayName || profile?.did}
-  {/if}
-  <span class="mx-1"></span> powered by <a
-    class="text-[var(--link-color)] hover:text-[var(--link-hover-color)]"
-    href="https://atproto.com/guides/glossary#at-protocol">atproto</a
-  >
-  <span class="mx-1"></span>
-  <a class="text-[var(--link-color)] hover:text-[var(--link-hover-color)]" href="/site/meta"
-    >more about this site</a
-  >
-  <span class="mx-1"></span>
-  <LastCommit />
-  <span class="mx-1"></span>
-  <TidClock />
-  {#if env.PUBLIC_ACTIVITYPUB_USER && env.PUBLIC_ACTIVITYPUB_USER.length > 0}
-    <a rel="me" href={`https://${env.PUBLIC_ACTIVITYPUB_USER.split('@')[2]}/@${env.PUBLIC_ACTIVITYPUB_USER.split('@')[1]}`} aria-label="ActivityPub actor link."></a>
-  {/if}
+<footer class="text-center py-4 text-primary text-sm opacity-60">
+  <div class="flex flex-col justify-center items-center gap-2">
+    <div>
+      <span>&copy; <span id="copyright-year"></span></span>
+
+      <span class="mx-1"></span>
+
+      {#if profile?.handle}
+        <a
+          href="https://bsky.app/profile/{profile.did}"
+          class="text-[var(--link-color)] hover:text-[var(--link-hover-color)]"
+        >
+          @{profile.handle}
+        </a>
+      {:else}
+        <span>{profile?.displayName || profile?.did}</span>
+      {/if}
+
+      {#if env.PUBLIC_ACTIVITYPUB_USER && env.PUBLIC_ACTIVITYPUB_USER.length > 0 && profile?.handle}
+        <span class="mx-1"></span>
+      {/if}
+
+      {#if env.PUBLIC_ACTIVITYPUB_USER && env.PUBLIC_ACTIVITYPUB_USER.length > 0}
+        <a
+          rel="me"
+          href={`https://${env.PUBLIC_ACTIVITYPUB_USER.split("@")[2]}/@${env.PUBLIC_ACTIVITYPUB_USER.split("@")[1]}`}
+          class="text-[var(--link-color)] hover:text-[var(--link-hover-color)]"
+        >
+          @{env.PUBLIC_ACTIVITYPUB_USER.split(
+            "@",
+          )[1]}@{env.PUBLIC_ACTIVITYPUB_USER.split("@")[2]}
+        </a>
+      {/if}
+    </div>
+
+    <div>
+      <span
+        >powered by <a
+          class="text-[var(--link-color)] hover:text-[var(--link-hover-color)]"
+          href="https://atproto.com/guides/glossary#at-protocol">atproto</a
+        ></span
+      >
+
+      <span class="mx-1"></span>
+      <a
+        class="text-[var(--link-color)] hover:text-[var(--link-hover-color)]"
+        href="/site/meta"
+      >
+        more about this site
+      </a>
+
+      <span class="mx-1"></span>
+      <LastCommit />
+
+      <span class="mx-1"></span>
+      <TidClock />
+    </div>
+  </div>
 </footer>
