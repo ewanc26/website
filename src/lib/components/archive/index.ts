f310ba7ad7a0aa99996fb1eac91ea700cebb40a4
Ewan Croft <git@ewancroft.uk>
2025-08-07 06:29:36 +0100

    add universal Blog Archive header
    - Remove StatsDisplay.svelte
    - Create ArchiveHeader.svelte

--- src/lib/components/archive/index.ts
+++ src/lib/components/archive/index.ts
@@ -2,4 +2,4 @@ export { default as YearTabs } from "./YearTabs.svelte";
 export { default as YearContent } from "./YearContent.svelte";
 export { default as MonthSection } from "./MonthSection.svelte";
 export { default as ArchiveCard } from "./ArchiveCard.svelte";
-export { default as StatsDisplay } from "./StatsDisplay.svelte";
+export { default as ArchiveHeader } from "./ArchiveHeader.svelte";
