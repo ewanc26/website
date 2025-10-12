7e55feba3823e6999414184285a45cde256fd480
Ewan Croft <git@ewancroft.uk>
2025-10-12 21:35:28 +0100

    RESET

--- src/lib/components/layout/main/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as DynamicLinks } from "./DynamicLinks.svelte";
-export { default as LatestBlogPost } from "./LatestBlogPost.svelte";
-export { default as ScrollToTop } from "./ScrollToTop.svelte";
--- src/routes/blog/+layout.ts
+++
@@ -1,8 +0,0 @@
-import { loadAllPosts } from "$services/blogService";
-
-export const prerender = false;
-export const trailingSlash = "never";
-
-export async function load({ fetch }) {
-  return await loadAllPosts(fetch);
-}
