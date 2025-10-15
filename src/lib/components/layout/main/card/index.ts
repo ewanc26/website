039c6fde0e7a96c1e0c69aa39c7bb7bb08ca1bd8
Ewan Croft <git@ewancroft.uk>
2025-10-15 21:07:36 +0100

    feat(card): export PostCard component

--- src/lib/components/layout/main/card/index.ts
+++ src/lib/components/layout/main/card/index.ts
@@ -1,3 +1,4 @@
 export { default as LinkCard } from './LinkCard.svelte';
 export { default as ProfileCard } from './ProfileCard.svelte';
 export { default as StatusCard } from './StatusCard.svelte';
+export { default as PostCard } from './PostCard.svelte';
