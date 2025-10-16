1e222058a0376082b06fbd54d19b26a8b1137420
Ewan <git@ewancroft.uk>
2025-10-16 01:47:18 +0100

    feat: export BlueskyPostCard component

--- src/lib/components/layout/main/card/index.ts
+++ src/lib/components/layout/main/card/index.ts
@@ -2,3 +2,4 @@ export { default as LinkCard } from './LinkCard.svelte';
 export { default as ProfileCard } from './ProfileCard.svelte';
 export { default as StatusCard } from './StatusCard.svelte';
 export { default as PostCard } from './PostCard.svelte';
+export { default as BlueskyPostCard } from './BlueskyPostCard.svelte';
