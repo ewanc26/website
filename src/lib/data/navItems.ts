112aa3bf215e28bf36bd86e67e571198f5250853
Ewan Croft <git@ewancroft.uk>
2025-10-17 18:27:15 +0100

    chore(nav): Remove blog link

--- src/lib/data/navItems.ts
+++ src/lib/data/navItems.ts
@@ -6,8 +6,6 @@ export interface NavItem {
 }
 
 export const navItems: NavItem[] = [
-    // FIX: Use the component export name directly
   { href: '/', label: 'Home', iconPath: 'Home' },
-  { href: 'https://blog.ewancroft.uk', label: 'Blog', iconPath: 'Book' },
   { href: '/site/meta', label: 'Site Meta', iconPath: 'Info' }
 ];
