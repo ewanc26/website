221a71d0a76e99fe4bd9de3341986fec44023d64
Ewan Croft <git@ewancroft.uk>
2025-10-19 18:24:25 +0100

    feat(Tangled): Add Tangled.org Git repository displays

--- src/lib/components/layout/main/card/index.ts
+++ src/lib/components/layout/main/card/index.ts
@@ -3,3 +3,4 @@ export { default as ProfileCard } from './ProfileCard.svelte';
 export { default as StatusCard } from './StatusCard.svelte';
 export { default as PostCard } from './PostCard.svelte';
 export { default as BlueskyPostCard } from './BlueskyPostCard.svelte';
+export { default as TangledRepoCard } from './TangledRepoCard.svelte';
