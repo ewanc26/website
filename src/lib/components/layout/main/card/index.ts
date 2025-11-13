bbb1aae56ad7b66d25e5bd69e211774b30657386
Ewan Croft <git@ewancroft.uk>
2025-11-13 21:53:35 +0000

    refactor: remove StatusCard component and retire now/ RSS/Atom routes

--- src/lib/components/layout/main/card/index.ts
+++ src/lib/components/layout/main/card/index.ts
@@ -1,6 +1,6 @@
 export { default as LinkCard } from './LinkCard.svelte';
 export { default as ProfileCard } from './ProfileCard.svelte';
-export { default as StatusCard } from './StatusCard.svelte';
 export { default as PostCard } from './PostCard.svelte';
 export { default as BlueskyPostCard } from './BlueskyPostCard.svelte';
 export { default as TangledRepoCard } from './TangledRepoCard.svelte';
+export { default as MusicStatusCard } from './MusicStatusCard.svelte';
