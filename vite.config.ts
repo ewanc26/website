3561a2631dfcf497ed9798bebf94087f25b85c9a
Ewan Croft <git@ewancroft.uk>
2026-03-24 03:09:49 +0000

    feat: automate version bump updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { version } from './package.json';
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
+	define: {
+		__APP_VERSION__: JSON.stringify(version)
+	},
 
 
 	build: {
