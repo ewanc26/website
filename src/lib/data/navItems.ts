cb08ec49a8397e8db4e8891d8db73ff9bfc9b6af
Ewan Croft <git@ewancroft.uk>
2026-04-11 14:01:42 +0100

    Revert "feat: add Work to navigation"
    This reverts commit 312b2bc0d8563d7d723cffa5a6b5a8f8d64c9227.

--- src/lib/data/navItems.ts
+++ src/lib/data/navItems.ts
@@ -4,7 +4,6 @@ export type { NavItem };
 
 export const navItems: NavItem[] = [
 	{ href: '/', label: 'Home', iconPath: 'Home' },
-	{ href: '/work', label: 'Work', iconPath: 'Briefcase' },
 	{ href: '/site/meta', label: 'Site Meta', iconPath: 'Info' },
 	{ href: '/github', label: 'GitHub', iconPath: 'Github' },
 	{ href: '/archive', label: 'Archive', iconPath: 'Archive' }
