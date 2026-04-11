eea805a7a1ce90ed1ba1e3f54419a931627b0e73
Ewan Croft <git@ewancroft.uk>
2026-04-11 13:40:46 +0100

    feat: add Work to navigation

--- src/lib/data/navItems.ts
+++ src/lib/data/navItems.ts
@@ -4,6 +4,7 @@ export type { NavItem };
 
 export const navItems: NavItem[] = [
 	{ href: '/', label: 'Home', iconPath: 'Home' },
+	{ href: '/work', label: 'Work', iconPath: 'Briefcase' },
 	{ href: '/site/meta', label: 'Site Meta', iconPath: 'Info' },
 	{ href: '/github', label: 'GitHub', iconPath: 'Github' },
 	{ href: '/archive', label: 'Archive', iconPath: 'Archive' }
