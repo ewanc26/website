76cdebdb6f9be4c601c63365f360ac9ea521a400
Ewan Croft <git@ewancroft.uk>
2026-05-30 22:20:29 +0100

    feat: add header and footer

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,6 @@
+<header style="padding: var(--space-md) 0; display: flex; justify-content: space-between; align-items: center; border-bottom: 1px solid var(--surface-color);">
+    <a href="/" style="font-weight: bold; text-decoration: none; color: inherit; font-size: var(--text-md);">Website</a>
+    <nav style="display: flex; gap: var(--space-md);">
+        <a href="/about" style="text-decoration: none; color: inherit; font-size: var(--text-sm);">About</a>
+    </nav>
+</header>
