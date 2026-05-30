8d3c964b7606f5ee89ec20f7f2a7793112966044
Ewan Croft <git@ewancroft.uk>
2026-05-30 22:44:59 +0100

    feat: populate about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import { SITE } from '$lib/config';
+</script>
+
+<main>
+    <header>
+        <h1 style="font-size: var(--text-xl);">About</h1>
+    </header>
+    <section>
+        <p style="font-size: var(--text-md);">
+            Welcome to the redesign of {SITE.title}. This site is an ongoing experiment 
+            in minimalist, systematic web design, focused on clarity, accessibility, 
+            and functional integrity.
+        </p>
+        <p style="font-size: var(--text-md);">
+            It is built to be a clear, performant view layer for my projects and 
+            technical explorations.
+        </p>
+    </section>
+</main>
