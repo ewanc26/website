733d660fca5809627cb7b5af4b0ecf95f6ef959a
Ewan Croft <git@ewancroft.uk>
2026-05-30 23:13:37 +0100

    fix: export fetchBlogPosts in service module

--- src/lib/services/atproto/fetch.ts
+++ src/lib/services/atproto/fetch.ts
@@ -1,6 +1,14 @@
-import { PUBLIC_ATPROTO_DID } from "$env/static/public";
-import { fetchKibunStatus as _fetchKibunStatus } from "@ewanc26/atproto";
+import { PUBLIC_ATPROTO_DID } from '$env/static/public';
+import { 
+    fetchKibunStatus as _fetchKibunStatus,
+    fetchBlogPosts as _fetchBlogPosts
+} from '@ewanc26/atproto';
 
 export async function fetchKibunStatus(fetchFn?: typeof fetch) {
-  return _fetchKibunStatus(PUBLIC_ATPROTO_DID, fetchFn);
+	return _fetchKibunStatus(PUBLIC_ATPROTO_DID, fetchFn);
 }
+
+export async function fetchBlogPosts(fetchFn?: typeof fetch) {
+	return _fetchBlogPosts(PUBLIC_ATPROTO_DID, fetchFn);
+}
+
