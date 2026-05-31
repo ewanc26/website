e46524f90b77bd5d223653ab26365f598ba2e011
Ewan Croft <git@ewancroft.uk>
2026-05-31 14:44:33 +0100

    feat: add blog and meta to navigation

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -3,4 +3,8 @@ export const SITE = {
   description: "A general-purpose site.",
 };
 
-export const NAV_LINKS = [{ label: "About", url: "/about" }];
+export const NAV_LINKS = [
+  { label: "About", url: "/about" },
+  { label: "Blog", url: "/blog" },
+  { label: "Meta", url: "/site/meta" },
+];
