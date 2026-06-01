041184e151f0597ef17bfdd78f2d2c8849ae84ef
Ewan Croft <git@ewancroft.uk>
2026-06-01 01:21:34 +0100

    refactor: fix event.fetch warning in about page

--- src/routes/about/+page.server.ts
+++ src/routes/about/+page.server.ts
@@ -11,7 +11,7 @@ import {
 } from "@ewanc26/atproto";
 import { PUBLIC_ATPROTO_DID } from "$env/static/public";
 
-export const load: PageServerLoad = async ({ fetch }) => {
+export const load: PageServerLoad = async () => {
   const [
     profile,
     links,
@@ -22,14 +22,14 @@ export const load: PageServerLoad = async ({ fetch }) => {
     sifaExternalAccounts,
     sifaProjects,
   ] = await Promise.all([
-    fetchProfile(PUBLIC_ATPROTO_DID, fetch),
-    fetchLinks(PUBLIC_ATPROTO_DID, fetch).catch(() => null),
-    fetchSifaProfile(PUBLIC_ATPROTO_DID, fetch).catch(() => null),
-    fetchSifaSkills(PUBLIC_ATPROTO_DID, fetch).catch(() => []),
-    fetchSifaEducation(PUBLIC_ATPROTO_DID, fetch).catch(() => []),
-    fetchSifaLanguages(PUBLIC_ATPROTO_DID, fetch).catch(() => []),
-    fetchSifaExternalAccounts(PUBLIC_ATPROTO_DID, fetch).catch(() => []),
-    fetchSifaProjects(PUBLIC_ATPROTO_DID, fetch).catch(() => []),
+    fetchProfile(PUBLIC_ATPROTO_DID),
+    fetchLinks(PUBLIC_ATPROTO_DID).catch(() => null),
+    fetchSifaProfile(PUBLIC_ATPROTO_DID).catch(() => null),
+    fetchSifaSkills(PUBLIC_ATPROTO_DID).catch(() => []),
+    fetchSifaEducation(PUBLIC_ATPROTO_DID).catch(() => []),
+    fetchSifaLanguages(PUBLIC_ATPROTO_DID).catch(() => []),
+    fetchSifaExternalAccounts(PUBLIC_ATPROTO_DID).catch(() => []),
+    fetchSifaProjects(PUBLIC_ATPROTO_DID).catch(() => []),
   ]);
 
   return {
