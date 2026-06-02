095581b266eefd283583d4f76baf86e5876b6679
Ewan Croft <git@ewancroft.uk>
2026-06-02 13:11:30 +0000

    feat: add empty states to reusable components
    - CommentSection.svelte: Show 'No comments yet' when empty
    - ProjectList.svelte: Show 'No projects available' when empty
    
    These components now handle missing data gracefully with brand-compliant messaging

--- src/lib/components/ProjectList.svelte
+++ src/lib/components/ProjectList.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import EmptyState from '$lib/components/EmptyState.svelte';
+
   interface Project {
     title: string;
     description: string;
@@ -8,13 +10,21 @@
   let { projects } = $props<{ projects: Project[] }>();
 </script>
 
-<ul class="project-list">
-  {#each projects as project}
-    <li>
-      <a href={project.url} class="project-item">
-        <h3 class="project-title">{project.title}</h3>
-        <p class="project-desc">{project.description}</p>
-      </a>
-    </li>
-  {/each}
-</ul>
+{#if projects.length > 0}
+  <ul class="project-list">
+    {#each projects as project}
+      <li>
+        <a href={project.url} class="project-item">
+          <h3 class="project-title">{project.title}</h3>
+          <p class="project-desc">{project.description}</p>
+        </a>
+      </li>
+    {/each}
+  </ul>
+{:else}
+  <EmptyState
+    title="No projects available"
+    description="Projects will appear here once data is loaded."
+    icon={false}
+  />
+{/if}
