b0dce1f92b17d8b2ce502f0dcaf04b321b54fc0d
Ewan Croft <git@ewancroft.uk>
2026-06-07 11:01:37 +0100

    feat(ui): update header and footer components for new egg locations

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -2,17 +2,31 @@
     import { SITE, NAV_LINKS } from '$lib/config';
     import { page } from '$app/state';
     import { X, Menu } from '@lucide/svelte';
-    import Triskele from './icons/Triskele.svelte';
-import { dev } from '$app/environment';
+    import TriskeleEgg from '$lib/components/ostara-eggs/TriskeleEgg.svelte';
+    import { dev } from '$app/environment';
+    import { onMount } from 'svelte';
 
     let isMenuOpen = $state(false);
+    let isMoonDay = $state(false);
+
+    onMount(() => {
+        isMoonDay = new Date().getDay() === 1;
+    });
 </script>
 
 <nav class="nav">
-    <a href="/" class="nav-brand">
-        <Triskele size={14} />
-{SITE.title}{#if dev}<span class="dev-chip">DEV</span>{/if}
-    </a>
+    <div class="nav-brand">
+        <span class="nav-brand-icon">
+            <TriskeleEgg size={14} />
+        </span>
+        <span class="nav-brand-name">
+            {SITE.title}
+            {#if isMoonDay}
+                <span class="moon-day-label">on mōnandæg</span>
+            {/if}
+            {#if dev}<span class="dev-chip"><br />DEV</span>{/if}
+        </span>
+    </div>
     
     <button class="menu-toggle" onclick={() => isMenuOpen = !isMenuOpen} aria-label="Toggle menu">
         {#if isMenuOpen}
