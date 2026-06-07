906bdeaff0d650f86e6e4035eb758902d69eee3c
Ewan Croft <git@ewancroft.uk>
2026-06-07 11:01:27 +0100

    feat(layout): integrate eggs into layout and error pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,9 @@
 	import Header from '$lib/components/Header.svelte';
 	import Footer from '$lib/components/Footer.svelte';
 	import SabbatBackground from '$lib/components/SabbatBackground.svelte';
+	import MondayEgg from '$lib/components/ostara-eggs/MondayEgg.svelte';
+	import IdleEgg from '$lib/components/ostara-eggs/IdleEgg.svelte';
+	import ThreeToast from '$lib/components/ostara-eggs/ThreeToast.svelte';
 	import './layout.css';
 
 	let { children } = $props();
@@ -12,6 +15,9 @@
 <svelte:head><link rel="icon" href="/favicon.svg" /></svelte:head>
 
 <SabbatBackground />
+<MondayEgg />
+<IdleEgg />
+<ThreeToast />
 <Header />
 {#if dev}<span class="dev-chip">DEV</span>{/if}
 <main class="shell-main">
