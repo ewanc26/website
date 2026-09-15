b3f35a9e2759a0106934ce6ab42fbc8cd42384e8
Ewan Croft <me@ewancroft.uk>
2026-09-15 11:49:22 +0100

    chore: remove Gaelic surname from about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -88,7 +88,7 @@
     {/if}
     <div class="about-intro">
       <h1 class="page-title">{profile.displayName ?? profile.handle}</h1>
-      <p class="about-gaelic">eòghann croit</p>
+      <p class="about-gaelic">eòghann</p>
       {#if profile.pronouns}
         <p class="about-pronouns">{profile.pronouns}</p>
       {/if}
